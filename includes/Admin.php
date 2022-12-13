3acb0279b122a39f3f9e2fe30e404982c74f1fd8
MHemelHasan <hemelsrgmts@gmail.com>
2022-12-13 17:00:57 +0600

    Add Plugin Info

--- includes/Admin.php
+++ includes/Admin.php
@@ -11,7 +11,10 @@ class Admin {
      * Initialize the class
      */
     function __construct() {
+        //Showing All Plugin that are active now
         // new Admin\WPMenu();
+
+        //Showing Download button and manage download function
         new Admin\WPPLdownload();
     }
 }
